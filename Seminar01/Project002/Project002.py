89e1b3fc91dff1f8bae948ae02ded321b50ae87d
Said Yarullin <said.98@mail.ru>
2022-09-27 16:27:53 +0300

    Project002 added

---
+++ Seminar01/Project002/Project002.py
@@ -0,0 +1,8 @@
+# Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+print('x  y  z  result')
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            result = not (x or y or z) == (not x and not y and not z)
+            print(f'{x}  {y}  {z}   {result}')
